4e4509a586d578924bb2f7de1e85cd02c6d5675a
JOANA DARC <caio.kiki95@gmail.com>
2023-03-30 20:27:01 -0300

    trigge_2

--- trigger_2.py
+++ trigger_2.py
@@ -1,21 +1,19 @@
+import ast
 import time
 import socket
-import keyboard
 import threading
 
 from random import randint
 
-chave = ['time1', 'time2', 'time3', 'time4']
 time_ganhador = []
 trigger_send_msg = False
-
+time_ganhador_atualizado = []
 
 ##############  SERVER ###################
 
 
 clients = []
-servers = [('localhost', 7777), ('localhost', 5000)]
-stop_threads = False
+servers_conectados = [('localhost', 7777)]
 first_exec_connect = True
 
 
@@ -29,32 +27,25 @@ def server_main():
         return print('\nNão foi possível iniciar o servidor!\n' + str(e))
 
     while True:
-        global stop_threads
-        if stop_threads:
-            break
         client, addr = server.accept()
         clients.append(client)
         thread = threading.Thread(target=messages_treatment, args=[client])
-        thread = threading.Thread(target=send_updates_to_client, args=[clients])
         thread.start()
 
 
-def send_updates_to_clients(clients):
-    while True:
-        global trigger_send_msg
-        if trigger_send_msg:
-            broadcast(str(time_ganhador).encode('utf-8'), clients)
-            trigger_send_msg = False
+# def send_updates_to_clients(clients):
+#     while True:
+#         global trigger_send_msg
+#         if trigger_send_msg:
+#             broadcast(str(time_ganhador).encode('utf-8'), clients)
+#             trigger_send_msg = False
 
 
 def messages_treatment(client):
     while True:
-        global stop_threads
-        if stop_threads:
-            break
         try:
             msg = client.recv(2048)
-            if 'Me mande a chave' in str(msg):
+            if 'Me mande a chave + lista de clientes' in str(msg):
                 update_chave(str(time_ganhador).encode('utf-8'), client)
             else:
                 broadcast(msg, client)
@@ -99,8 +90,11 @@ def client_main(ip, host):
     time.sleep(3)
     username = str(randint(0, 100))
 
+    global first_exec_connect
     if first_exec_connect:
-        request = 'Me mande a chave'
+        request = 'Me mande a chave + lista de clientes'
+        first_exec_connect = False
+
     else:
         request = None
 
@@ -115,25 +109,22 @@ def client_main(ip, host):
 
 def receive_messages(client):
     while True:
-        global stop_threads
-        if stop_threads:
-            break
         try:
             msg = client.recv(2048).decode('utf-8')
             if msg:
-                print(msg + '\n')
-        except:
+                global time_ganhador_atualizado
+                time_ganhador_atualizado = ast.literal_eval(msg)
+                print('Atualização: ' + str(time_ganhador_atualizado) + '\n')
+        except Exception as e:
             print('\nNão foi possível permanacer conctado no servidor!\n')
             print('Precione <ENTER> para continuar...')
+            print(e)
             client.close()
             break
 
 
 def send_messages(client, username, message=None):
     while True:
-        global stop_threads
-        if stop_threads:
-            break
         try:
             if message:
                 client.send(f'<{username}> {message}'.encode('utf-8'))
@@ -144,50 +135,30 @@ def send_messages(client, username, message=None):
 
 ##############  ALL ###################
 
-
-def close_theads():
-    while True:
-        global stop_threads
-        if stop_threads:
-            break
-        try:
-            if keyboard.is_pressed('esc'):  # if key 'esc' is pressed
-                print('Closing (maybe is required you to press ENTER to finish)')
-                stop_threads = True
-        except:
-            pass
-
-
 def run_champ():
     contador = 0
     while True:
-        global stop_threads
         global trigger_send_msg
-        if stop_threads:
-            break
-        time.sleep(10)
+        time.sleep(1)
         contador += 1
         time_ganhador.append('Random ' + str(contador))
         trigger_send_msg = True
 
 
-thread_close = threading.Thread(target=close_theads)
 thread_server = threading.Thread(target=server_main)
 
 thread_champ = threading.Thread(target=run_champ)
 thread_client = threading.Thread(target=client_main, args=[None, 7777])
-thread_client2 = threading.Thread(target=client_main, args=[None, 7777])
+# thread_client2 = threading.Thread(target=client_main, args=[None, 7777])
 
-
-thread_close.start()
-time.sleep(2)
-thread_server.start()
-time.sleep(2)
-thread_client.start()
 
 thread_champ.start()
-time.sleep(15)
-thread_client2.start()
+thread_server.start()
+time.sleep(5)
+thread_client.start()
+
+# time.sleep(15)
+# thread_client2.start()
 
 thread_server.join()
 thread_client.join()
